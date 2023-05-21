cd2c3a08829ad5e7b10dcc83d7c9db28a9a36ac1
pr1203 <123905902+pr1203@users.noreply.github.com>
2023-05-20 21:34:21 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,31 +1,20 @@
-DROP TABLE IF EXISTS `Students`;
-CREATE TABLE `Students`(
-    student_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL
-    );
+DROP TABLE IF EXISTS
 
-DROP TABLE IF EXISTS `Quizzes`;
-CREATE TABLE `Quizzes`(
-    quiz_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    subject TEXT NOT NULL,
-    number_of_questions INT NOT NULL,
-    quiz_date DATE NOT NULL
-    );
+CREATE TABLE students (
+  student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  firstname TEXT NOT NULL,
+  lastname TEXT NOT NULL,
+);
 
-DROP TABLE IF EXISTS `Quiz_Results`;
-CREATE TABLE `Quiz_Results`(
-    result_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    result INTEGER NOT NULL,
-    quiz_id INTEGER NOT NULL,
-    student_id INTEGER NOT NULL
-    );
+CREATE TABLE quizzes (
+  quiz_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  subject TEXT NOT NULL,
+  total_questions TEXT NOT NULL,
+  date_given
+);
 
-INSERT INTO 'Students' (student_id, first_name, last_name) VALUES
-(1, 'John', 'Smith');
-
-INSERT INTO 'Quizzes' (quiz_id, subject, number_of_questions, quiz_date) VALUES
-(1, 'Python Basics', 5, '2015-05-05');
-
-INSERT INTO 'Quiz_Results' (result, quiz_id, student_id) VALUES
-(85, 1, 1);
+CREATE TABLE student_scores (
+  student_id INTEGER NOT NULL,
+  quiz_id INTEGER NOT NULL,
+  score INTEGER NOT NULL
+);
